98a88033715becad59d453fd38a679b21e827c59
Jaireell Son Regala <regalajaireellson@gmail.com>
2025-09-11 00:38:00 +0800

    activity 2 - Hello! Regala, Jaireell Son S.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/RegalaJaireellSonAct2.html');
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
